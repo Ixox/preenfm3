773366bc0adbf624b598405e0eff8c5c8192dbd5
pvig <patricevigouroux@yahoo.fr>
2022-11-01 23:17:26 +0100

    usr files load fix

--- firmware/Src/filesystem/UserEnvCurve.cpp
+++ firmware/Src/filesystem/UserEnvCurve.cpp
@@ -91,18 +91,14 @@ void UserEnvCurve::loadUserEnvCurveFromBin(int f, const char* fileName) {
     load(fileName, 4, &numberOfSample, 2);
 
     int sampleSize = numberOfSample * 4;
-    if (sampleSize <= 64) {
-        load(fileName, 6, userEnvCurves[f], sampleSize);
-    }
+    load(fileName, 6, userEnvCurves[f], sampleSize);
 }
 
 void UserEnvCurve::saveUserEnvCurveToBin(int f, const char* fileName) {
     save(fileName, 0, userEnvCurveNames[f], 4);
     save(fileName, 4, &numberOfSample, 2);
     int sampleSize = numberOfSample * 4;
-    if (sampleSize <= 64) {
-        save(fileName, 6, userEnvCurves[f], sampleSize);
-    }
+    save(fileName, 6, userEnvCurves[f], sampleSize);
 }
 
 void UserEnvCurve::loadUserEnvCurveFromTxt(int f, const char* fileName, int size) {
