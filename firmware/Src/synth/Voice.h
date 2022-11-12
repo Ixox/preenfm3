bd9c5cbc7524d860642c5c3853f3fabd16994667
Ixox <xavier.hosxe@gmail.com>
2022-11-12 19:20:14 +0100

    LP on mix change when in MPE mode

--- firmware/Src/synth/Voice.h
+++ firmware/Src/synth/Voice.h
@@ -152,37 +152,51 @@ public:
         float inv65535 = .0000152587890625; // 1/ 65535
         int pan;
         int numberOfMix = algoInformation[(int) (currentTimbre->getParamRaw()->engine1.algo)].mix;
+        // add a LP on mix when MPE to mitigate noise dur to midi CC being [0-127]
+        bool notMPE = currentTimbre->timbreNumber_ != 0 || currentTimbre->getMPESetting() == 0;
 
 
         mix1 = currentTimbre->getParamRaw()->engineMix1.mixOsc1 + matrix.getDestination(MIX_OSC1) + matrix.getDestination(ALL_MIX);
         // Optimization to check mix1 is between 0 and 1
-        mix1 = __USAT((int)((mix1 * .05 + mix1Previous * .95) * 65536) , 16) * inv65535;
-        mix1Previous = mix1;
+        if (likely(notMPE)) {
+            mix1 = __USAT((int)(mix1 * 65536) , 16) * inv65535;
+        } else {
+            mix1 = __USAT((int)((mix1 * .05 + mix1Previous * .95) * 65536) , 16) * inv65535;
+            mix1Previous = mix1;
+        }
         float pan1 = currentTimbre->getParamRaw()->engineMix1.panOsc1 + matrix.getDestination(PAN_OSC1) + matrix.getDestination(ALL_PAN) + 1.0f;
         // pan1 is between -1 and 1 : Scale from 0.0 to 256
         pan = __USAT((int )(pan1 * 128), 8);
         pan1Left = panTable[pan] * .05f + pan1Left * .95f;
         pan1Right = panTable[256 - pan] * .05f + pan1Right * .95f;
 
-        if (numberOfMix == 1) {
+        if (unlikely(numberOfMix == 1)) {
             return;
         }
 
         mix2 = currentTimbre->getParamRaw()->engineMix1.mixOsc2 + matrix.getDestination(MIX_OSC2) + matrix.getDestination(ALL_MIX);
-        mix2 = __USAT((int)((mix2 * .05 + mix2Previous * .95) * 65535) , 16) * inv65535;
-        mix2Previous = mix2;
+        if (likely(notMPE)) {
+            mix2 = __USAT((int)(mix2 * 65535) , 16) * inv65535;
+        } else {
+            mix2 = __USAT((int)((mix2 * .05 + mix2Previous * .95) * 65535) , 16) * inv65535;
+            mix2Previous = mix2;
+        }
         float pan2 = currentTimbre->getParamRaw()->engineMix1.panOsc2 + matrix.getDestination(PAN_OSC2) + matrix.getDestination(ALL_PAN) + 1.0f;
         pan = __USAT((int )(pan2 * 128), 8);
         pan2Left = panTable[pan] * .05f + pan2Left * .95f;
         pan2Right = panTable[256 - pan] * .05f + pan2Right * .95f;
 
-        if (numberOfMix == 2) {
+        if (unlikely(numberOfMix == 2)) {
             return;
         }
 
         mix3 = currentTimbre->getParamRaw()->engineMix2.mixOsc3 + matrix.getDestination(MIX_OSC3) + matrix.getDestination(ALL_MIX);
-        mix3 = __USAT((int)((mix3 * .05 + mix3Previous * .95) * 65535) , 16) * inv65535;
-        mix3Previous = mix3;
+        if (likely(notMPE)) {
+            mix3 = __USAT((int)(mix3 * 65535) , 16) * inv65535;
+        } else {
+            mix3 = __USAT((int)((mix3 * .05 + mix3Previous * .95) * 65535) , 16) * inv65535;
+            mix3Previous = mix3;
+        }
         float pan3 = currentTimbre->getParamRaw()->engineMix2.panOsc3 + matrix.getDestination(PAN_OSC3) + matrix.getDestination(ALL_PAN) + 1.0f;
         pan = __USAT((int )(pan3 * 128), 8);
         pan3Left = panTable[pan] * .05f + pan3Left * .95f;
@@ -194,8 +208,12 @@ public:
 
         // No matrix for mix4 and pan4
         mix4 = currentTimbre->getParamRaw()->engineMix2.mixOsc4 + matrix.getDestination(MIX_OSC4) + matrix.getDestination(ALL_MIX);
-        mix4 = __USAT((int)((mix4 * .05 + mix4Previous * .95) * 65535) , 16) * inv65535;
-        mix4Previous = mix4;
+        if (likely(notMPE)) {
+            mix4 = __USAT((int)(mix4 * 65535) , 16) * inv65535;
+        } else {
+            mix4 = __USAT((int)((mix4 * .05 + mix4Previous * .95) * 65535) , 16) * inv65535;
+            mix4Previous = mix4;
+        }
         float pan4 = currentTimbre->getParamRaw()->engineMix2.panOsc4 + matrix.getDestination(PAN_OSC4) + matrix.getDestination(ALL_PAN) + 1.0f;
         pan = __USAT((int )(pan4 * 128), 8);
         pan4Left = panTable[pan] * .05f + pan4Left * .95f;
@@ -206,8 +224,12 @@ public:
         }
 
         mix5 = currentTimbre->getParamRaw()->engineMix3.mixOsc5 + matrix.getDestination(ALL_MIX);
-        mix5 = __USAT((int)((mix5 * .05 + mix5Previous * .95) * 65535) , 16) * inv65535;
-        mix5Previous = mix5;
+        if (likely(notMPE)) {
+            mix5 = __USAT((int)(mix5 * 65535) , 16) * inv65535;
+        } else {
+            mix5 = __USAT((int)((mix5 * .05 + mix5Previous * .95) * 65535) , 16) * inv65535;
+            mix5Previous = mix5;
+        }
 
         float pan5 = currentTimbre->getParamRaw()->engineMix3.panOsc5 + matrix.getDestination(ALL_PAN) + 1.0f;
         pan = __USAT((int )(pan5 * 128), 8);
@@ -215,8 +237,12 @@ public:
         pan5Right = panTable[256 - pan] * .05f + pan5Right * .95f;
 
         mix6 = currentTimbre->getParamRaw()->engineMix3.mixOsc6 + matrix.getDestination(ALL_MIX);
-        mix6 = __USAT((int)((mix6 * .05 + mix6Previous * .95) * 65535) , 16) * inv65535;
-        mix6Previous = mix6;
+        if (likely(notMPE)) {
+            mix6 = __USAT((int)(mix6 * 65535) , 16) * inv65535;
+        } else {
+            mix6 = __USAT((int)((mix6 * .05 + mix6Previous * .95) * 65535) , 16) * inv65535;
+            mix6Previous = mix6;
+        }
 
         float pan6 = currentTimbre->getParamRaw()->engineMix3.panOsc6 + matrix.getDestination(ALL_PAN) + 1.0f;
         pan = __USAT((int )(pan6 * 128), 8);
